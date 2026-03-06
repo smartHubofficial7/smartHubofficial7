0ad7a3d244427d7eb54a6fa4b1ccfdafd0a3ced7
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-06 17:16:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,60 @@
 
+let cart=[]
+
+function addCart(item){
+
+cart.push(item)
+
+document.getElementById("cartCount").innerText=cart.length
+
+document.getElementById("cartItems").innerHTML+=`<li>${item}</li>`
+
+showNotify(item+" added to cart")
+
+}
+
+function showNotify(text){
+
+let n=document.getElementById("notify")
+
+n.innerText=text
+
+n.style.display="block"
+
+setTimeout(()=>{
+
+n.style.display="none"
+
+},2000)
+
+}
+
+function openCart(){
+
+document.getElementById("cartPanel").style.right="0"
+
+}
+
+function buy(){
+
+document.getElementById("paymentBox").style.display="block"
+
+}
+
+function closePay(){
+
+document.getElementById("paymentBox").style.display="none"
+
+}
+
+function openLogin(){
+
+document.getElementById("loginBox").style.display="block"
+
+}
+
+function closeLogin(){
+
+document.getElementById("loginBox").style.display="none"
+
+}
