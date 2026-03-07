1bee07576f6c78007dec417e6885c51bf7115e4b
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-07 18:15:07 +0530

    Update cart.js

--- cart.js
+++ cart.js
@@ -1,57 +1,19 @@
-let cart = JSON.parse(localStorage.getItem("cart")) || []
+let cart=JSON.parse(localStorage.getItem("cart"))||[]
 
-const container=document.getElementById("cart-items")
+let container=document.getElementById("cartItems")
 
 let total=0
 
-function displayCart(){
-
-container.innerHTML=""
-
-cart.forEach((item,index)=>{
-
-total+=item.price
+cart.forEach(item=>{
 
 container.innerHTML+=`
 
-<div class="product">
-
-<img src="${item.image}">
-
-<h3>${item.name}</h3>
-
-<p>$${item.price}</p>
-
-<button onclick="removeItem(${index})">Remove</button>
-
-</div>
+<p>${item.name} - ₹${item.price}</p>
 
 `
 
+total+=item.price
+
 })
 
-document.getElementById("total").innerText="Total: $"+total
-
-}
-
-function removeItem(i){
-
-cart.splice(i,1)
-
-localStorage.setItem("cart",JSON.stringify(cart))
-
-location.reload()
-
-}
-
-function checkout(){
-
-alert("Order placed successfully!")
-
-localStorage.removeItem("cart")
-
-location.reload()
-
-}
-
-displayCart()
+container.innerHTML+=`<h2>Total ₹${total}</h2>`
