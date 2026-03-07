821425c28a253529f5e68a419b53d8b1a744be81
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-07 19:26:08 +0530

    Update product.js

--- product.js
+++ product.js
@@ -1,7 +1,33 @@
-document.getElementById("product").innerHTML=`
+fetch("products.json")
 
-<h2>Sample Product</h2>
-<p>Price ₹1000</p>
-<button>Add to Cart</button>
+.then(res => res.json())
 
-`
+.then(data => {
+
+let container = document.getElementById("products");
+
+data.forEach(product => {
+
+let div = document.createElement("div");
+
+div.className = "product";
+
+div.innerHTML = `
+
+<img src="${product.image}">
+
+<h3>${product.name}</h3>
+
+<p>₹${product.price}</p>
+
+<button onclick="addToCart('${product.name}',${product.price})">
+Add to Cart
+</button>
+
+`;
+
+container.appendChild(div);
+
+});
+
+});
