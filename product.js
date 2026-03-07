4d21e4f99cb973f15e9c3f1d359f4fd6034923b2
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-07 19:42:03 +0530

    Update product.js

--- product.js
+++ product.js
@@ -10,18 +10,18 @@ data.forEach(product => {
 
 let div = document.createElement("div");
 
-div.className = "product";
+div.className="product";
 
-div.innerHTML = `
+div.innerHTML=`
 
 <img src="${product.image}">
-
 <h3>${product.name}</h3>
-
 <p>₹${product.price}</p>
 
 <button onclick="addToCart('${product.name}',${product.price})">
+
 Add to Cart
+
 </button>
 
 `;
