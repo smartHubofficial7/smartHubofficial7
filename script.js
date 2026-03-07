d3986e0a1f710faa60159f9ab7c21c354ac263b8
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-07 19:06:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,114 +1,11 @@
-let products = [
+let cart=[];
 
-{
-id:1,
-name:"Wireless Earbuds",
-price:999,
-image:"https://via.placeholder.com/200"
-},
+function addToCart(name,price){
 
-{
-id:2,
-name:"Smart Watch",
-price:1999,
-image:"https://via.placeholder.com/200"
-},
-
-{
-id:3,
-name:"Bluetooth Speaker",
-price:1499,
-image:"https://via.placeholder.com/200"
-}
-
-];
-
-function showProducts(){
-
-let list = document.getElementById("product-list");
-
-list.innerHTML="";
-
-products.forEach(p=>{
-
-list.innerHTML+=`
-
-<div class="product">
-
-<img src="${p.image}">
-
-<h3>${p.name}</h3>
-
-<p>₹${p.price}</p>
-
-<button onclick="addToCart(${p.id})">
-Add To Cart
-</button>
-
-</div>
-
-`;
-
-});
-
-}
-
-showProducts();
-
-function addToCart(id){
-
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-let product = products.find(p=>p.id==id);
-
-cart.push(product);
+cart.push({name,price});
 
 localStorage.setItem("cart",JSON.stringify(cart));
 
-updateCart();
-
-}
-
-function updateCart(){
-
-let cart = JSON.parse(localStorage.getItem("cart")) || [];
-
-document.getElementById("cart-count").innerText = cart.length;
-
-}
-
-updateCart();
-
-function searchProduct(){
-
-let search = document.getElementById("search").value.toLowerCase();
-
-let filtered = products.filter(p=>p.name.toLowerCase().includes(search));
-
-let list = document.getElementById("product-list");
-
-list.innerHTML="";
-
-filtered.forEach(p=>{
-
-list.innerHTML+=`
-
-<div class="product">
-
-<img src="${p.image}">
-
-<h3>${p.name}</h3>
-
-<p>₹${p.price}</p>
-
-<button onclick="addToCart(${p.id})">
-Add To Cart
-</button>
-
-</div>
-
-`;
-
-});
+alert("Added to cart");
 
 }
