07bc3bf715b1efb7ea14ce1578540a2b435558e1
smartHubofficial7 <smarthubofficial777@gmail.com>
2026-03-07 18:14:38 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,21 @@
-const products = [
+fetch("products.json")
+.then(res=>res.json())
+.then(data=>{
 
-{
-name:"Gaming Laptop",
-price:1200,
-image:"https://images.unsplash.com/photo-1517336714731-489689fd1ca8"
-},
+let container=document.getElementById("products")
 
-{
-name:"Smart Phone",
-price:700,
-image:"https://images.unsplash.com/photo-1511707171634-5f897ff02aa9"
-},
-
-{
-name:"Smart Watch",
-price:150,
-image:"https://images.unsplash.com/photo-1518443895914-3c5f4eaa3c5d"
-},
-
-{
-name:"Headphones",
-price:80,
-image:"https://images.unsplash.com/photo-1585386959984-a4155224a1ad"
-}
-
-]
-
-let cart = JSON.parse(localStorage.getItem("cart")) || []
-let wishlist = JSON.parse(localStorage.getItem("wishlist")) || []
-
-
-function displayProducts(list){
-
-const container=document.getElementById("product-list")
-
-container.innerHTML=""
-
-list.forEach((p,i)=>{
+data.forEach(product=>{
 
 container.innerHTML+=`
 
 <div class="product">
 
-<img src="${p.image}">
+<h3>${product.name}</h3>
+<p>₹${product.price}</p>
 
-<h3>${p.name}</h3>
-
-<p>$${p.price}</p>
-
-<button onclick="addCart(${i})">Add to Cart</button>
-
-<button onclick="addWishlist(${i})">❤️</button>
+<button onclick="addCart('${product.name}',${product.price})">
+Add to Cart
+</button>
 
 </div>
 
@@ -58,58 +23,16 @@ container.innerHTML+=`
 
 })
 
-}
+})
 
-function addCart(i){
+function addCart(name,price){
 
-cart.push(products[i])
+let cart=JSON.parse(localStorage.getItem("cart"))||[]
+
+cart.push({name,price})
 
 localStorage.setItem("cart",JSON.stringify(cart))
-
-updateCart()
 
 alert("Added to cart")
 
 }
-
-function addWishlist(i){
-
-wishlist.push(products[i])
-
-localStorage.setItem("wishlist",JSON.stringify(wishlist))
-
-alert("Added to wishlist ❤️")
-
-}
-
-function updateCart(){
-
-document.getElementById("cart-count").innerText=cart.length
-
-}
-
-document.getElementById("search").addEventListener("keyup",function(){
-
-let value=this.value.toLowerCase()
-
-let filtered=products.filter(p=>p.name.toLowerCase().includes(value))
-
-displayProducts(filtered)
-
-})
-
-function toggleDark(){
-
-document.body.classList.toggle("dark")
-
-}
-
-function toggleMenu(){
-
-document.getElementById("menu").classList.toggle("active")
-
-}
-
-displayProducts(products)
-
-updateCart()
